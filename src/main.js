202b2ebf46778f631c651020c1bfa5f354515fd2
Tony <anton-rus@mail.ru>
2020-11-19 12:44:55 +0300

    1-3. Рефакторинг, 1

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import {createPointAddTemplate} from './view/point-add';
 import {createPointEditTemplate} from './view/point-edit';
 import {createPointTemplate} from './view/point';
 
-const POINT_COUNT = 3;
+const Place = {
+  BEFOREBEGIN: `beforebegin`,
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`
+};
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -14,23 +19,23 @@ const render = (container, template, place) => {
 
 const tripMainElement = document.querySelector(`.trip-main`);
 
-render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
+render(tripMainElement, createTripInfoTemplate(), Place.AFTERBEGIN);
 
 const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
 const tripControlsTitleElements = tripControlsElement.querySelectorAll(`h2`);
 
-render(tripControlsTitleElements[0], createTripMenuTemplate(), `afterend`);
-render(tripControlsTitleElements[1], createTripFiltersTemplate(), `afterend`);
+render(tripControlsTitleElements[0], createTripMenuTemplate(), Place.AFTEREND);
+render(tripControlsTitleElements[1], createTripFiltersTemplate(), Place.AFTEREND);
 
 const tripEventsElement = document.querySelector(`.trip-events`);
 
-render(tripEventsElement, createTripEventsTemplate(), `beforeend`);
+render(tripEventsElement, createTripEventsTemplate(), Place.BEFOREEND);
 
 const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
 
-render(tripEventsListElement, createPointEditTemplate(), `beforeend`);
-render(tripEventsListElement, createPointAddTemplate(), `beforeend`);
+render(tripEventsListElement, createPointEditTemplate(), Place.BEFOREEND);
+render(tripEventsListElement, createPointAddTemplate(), Place.BEFOREEND);
 
-for (let i = 0; i < POINT_COUNT; i++) {
-  render(tripEventsListElement, createPointTemplate(), `beforeend`);
+for (let i = 0; i < createPointTemplate().length; i++) {
+  render(tripEventsListElement, createPointTemplate()[i], Place.BEFOREEND);
 }
